ac8048c4628680c16e22111a4cb5180a591b98e9
Sunnyshineshow <112595512+SIIT-Sunnyshineshow@users.noreply.github.com>
2022-11-20 18:22:53 +0700

    SQL modified

--- librarian.sql
+++ librarian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 20, 2022 at 08:09 AM
+-- Generation Time: Nov 20, 2022 at 12:22 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -32,8 +32,52 @@ CREATE TABLE `book` (
   `book_name` text NOT NULL,
   `author` text NOT NULL,
   `genre_id` int(11) NOT NULL,
+  `book_img` text NOT NULL,
   `date_added` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`book_id`, `book_name`, `author`, `genre_id`, `book_img`, `date_added`) VALUES
+(1, 'Harry Potter and the Philosopher\'s Stone', 'J. K. Rowling', 4, '', '2022-11-20'),
+(2, 'Harry Potter and the Chamber of Secrets', 'J. K. Rowling', 4, '', '2022-11-20'),
+(3, 'Harry Potter and the Prisoner of Azkaban', 'J. K. Rowling', 4, '', '2022-11-20'),
+(4, 'Harry Potter and the Goblet of Fire', 'J. K. Rowling', 4, '', '2022-11-20'),
+(5, 'Harry Potter and the Order of the Phoenix', 'J. K. Rowling', 4, '', '2022-11-20'),
+(6, 'Harry Potter and the Half-Blood Prince', 'J. K. Rowling', 4, '', '2022-11-20'),
+(7, 'Harry Potter and the Deathly Hallows', 'J. K. Rowling', 4, '', '2022-11-20'),
+(8, 'Fire & Blood', 'George R. R. Martin', 4, '', '2022-11-20'),
+(9, 'A Game of Thrones', 'George R. R. Martin', 4, '', '2022-11-20'),
+(10, 'A Clash of Kings', 'George R. R. Martin', 4, '', '2022-11-20'),
+(11, 'A Storm of Swords', 'George R. R. Martin', 4, '', '2022-11-20'),
+(12, 'A Feast for Crows', 'George R. R. Martin', 4, '', '2022-11-20'),
+(13, 'A Dance with Dragons', 'George R. R. Martin', 4, '', '2022-11-20'),
+(14, 'The Winds of Winter', 'George R. R. Martin', 4, '', '2022-11-20'),
+(15, 'A Dream of Spring', 'George R. R. Martin', 4, '', '2022-11-20'),
+(16, 'The Sun and Her Flowers', 'Rupi Kaur', 9, '', '2022-11-20'),
+(17, 'Pillow Thoughts', 'Courtney Peppernell', 9, '', '2022-11-20'),
+(18, 'Milk honey', 'Rupi Kaur', 9, '', '2022-11-20'),
+(19, 'Call Us What We Carry', 'Amanda Gorman', 9, '', '2022-11-20'),
+(20, 'Devotions: The Selected Poems of Mary Oliver', 'Mary Oliver', 9, '', '2022-11-20'),
+(21, 'Home Body', 'Rupi Kaur', 9, '', '2022-11-20'),
+(22, 'Dracula', 'Bram Stoker', 6, '', '2022-11-20'),
+(23, 'Frankenstein', 'Mary Shelley', 6, '', '2022-11-20'),
+(24, 'The Haunting of Hill House', 'Shirley Jackson', 6, '', '2022-11-20'),
+(25, 'The Shining', 'Stephen King', 6, '', '2022-11-20'),
+(26, 'It', 'Stephen King', 6, '', '2022-11-20'),
+(27, 'Interview with the Vampire', 'Anne Rice', 6, '', '2022-11-20'),
+(28, 'House of Leaves', 'Mark Z. Danielewski', 6, '', '2022-11-20'),
+(29, 'Something Wicked This Way Comes', 'Ray Bradbury', 6, '', '2022-11-20'),
+(30, 'A Light in the Flame: A Flesh and Fire Novel', 'Jennifer L. Armentrout', 7, '', '2022-11-20'),
+(31, 'Red, White & Royal Blue', 'Casey McQuiston', 7, '', '2022-11-20'),
+(32, 'It Ends with Us', 'Colleen Hoover', 7, '', '2022-11-20'),
+(33, 'Guns, Germs, and Steel', 'Jared Diamond', 5, '', '2022-11-20'),
+(34, '1776', 'David McCullough', 5, '', '2022-11-20'),
+(35, 'Short Stories in German for Beginners', 'Alex Rawlings, Olly Richards', 10, '', '2022-11-20'),
+(36, 'The Language Instinct', 'Steven Pinker', 10, '', '2022-11-20'),
+(37, 'How to Learn a Foreign Language', 'Rosetta Stone', 10, '', '2022-11-20');
 
 -- --------------------------------------------------------
 
@@ -86,6 +130,22 @@ CREATE TABLE `genre` (
   `genre_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`genre_id`, `genre_name`) VALUES
+(1, 'Action&Adventure'),
+(2, 'Comic'),
+(3, 'Detective'),
+(4, 'Fantasy'),
+(5, 'Historical'),
+(6, 'Horror '),
+(7, 'Romance'),
+(8, 'Memor'),
+(9, 'Poetry'),
+(10, 'Language ');
+
 -- --------------------------------------------------------
 
 --
@@ -121,6 +181,13 @@ CREATE TABLE `member` (
   `user_id` int(11) NOT NULL,
   `expire_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`member_num`, `user_id`, `expire_date`) VALUES
+(1, 7, '2024-02-24');
 
 -- --------------------------------------------------------
 
@@ -169,12 +236,8 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`user_id`, `username`, `password`, `fname`, `lname`, `tel_no`) VALUES
 (1, '6322772359', '*1186EE5EFB03BB1F6B03EC94BA8673B3B4595854', 'Napon', '(Sunnyshineshow)', '+6695741822'),
 (7, 'alicerayne', '*70DD2137A2F66BB4DDEF695BB013ACF9E823E035', 'Alice', 'Rayne', '+6699547896'),
-(8, 'alicerayne', '*70DD2137A2F66BB4DDEF695BB013ACF9E823E035', 'Alice', 'Rayne', '+6699547896'),
-(9, 'CCCC', '*2AE61F4B9BF9BDD4569C858023A5CF1988FF1E56', 'A', 'B', '+6695741822'),
-(10, 'CCCC', '*2AE61F4B9BF9BDD4569C858023A5CF1988FF1E56', 'A', 'B', '+6695741822'),
-(11, 'CCCC', '*2AE61F4B9BF9BDD4569C858023A5CF1988FF1E56', 'A', 'B', '+6695741822'),
-(12, 'CCCC', '*2AE61F4B9BF9BDD4569C858023A5CF1988FF1E56', 'A', 'B', '+6695741822'),
-(13, 'CCCC', '*2AE61F4B9BF9BDD4569C858023A5CF1988FF1E56', 'A', 'B', '+6695741822');
+(20, 'CCC', '*C165DDE46E7436332DA62278FF8A044F0EED8BF1', 'A', 'B', '+6695741822'),
+(21, 'AAA', '*5AF9D0EA5F6406FB0EDD0507F81C1D5CEBE8AC9C', 'AA', 'AA', '0008880858');
 
 --
 -- Indexes for dumped tables
@@ -266,7 +329,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `bookshelf`
@@ -284,7 +347,7 @@ ALTER TABLE `borrowed_book`
 -- AUTO_INCREMENT for table `genre`
 --
 ALTER TABLE `genre`
-  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `librarian`
@@ -296,7 +359,7 @@ ALTER TABLE `librarian`
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `member_num` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `member_num` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `returned_book`
@@ -308,7 +371,7 @@ ALTER TABLE `returned_book`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
